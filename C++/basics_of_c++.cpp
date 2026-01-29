277d3bf0b1ba8b87f57f73e9310098cc03019c4d
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-01-29 22:10:43 +0500

    "basics_of_c++"

---
+++ C++/basics_of_c++.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout<<"hello"<<'\n';
+
+    // int x = 4;
+    // int y = 6;
+    // int sum = x+y;
+    // cout<<sum;
+
+    // integers (including whole numbers)
+    // int age = 15;
+    // int year  = 2026;
+    // int day = 7.5;    // it would show error because 7.5 is not a whole number
+    //  cout<< day<<endl;
+
+    // double (including decimal numbers)
+    //  double gpa = 3.99;
+    //  double price = 999.99;
+    //  cout<< price<<endl;
+
+    // char (stores single character)
+    //  char grade = 'A';
+    //  char worng = 'xyz';   // it will only print the last character
+
+    //  cout<<worng<<endl;
+
+    // boolean  (gives   true or false value )
+    //  bool student = false;
+    //  bool forSale = true;
+
+    //  cout<<forSale<<endl;
+
+    //  String (Objects that reprsents a sequence of text);
+        string name = "umar";
+        string something ="ljfdsk";
+
+        cout<<something<<endl;
+        cout<<"Hello "<<name<<endl;
+
+    return 0;
+}
