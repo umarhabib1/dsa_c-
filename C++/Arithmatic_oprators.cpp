503d7d64e9ec733756acb8f28ce637705b9259a5
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-02-03 22:42:51 +0500

    "Arithmatic_oprators"

---
+++ C++/Arithmatic_oprators.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Arithmatic oprators are:  + , -,*,/,%,++,--,+=,*=,%=,-= ........... etc;
+    int student = 23;
+    // student = 23+1;
+    // student+=1;
+    // student++;
+    // cout<<student<<endl;
+
+    // student = 23-1;
+    // student-=1;
+    // student--;
+    // cout<<student<<endl;
+
+
+    // student = 23*1;
+    // student*=2;
+    // cout<<student<<endl;
+
+    // student = student/2;
+    // student /= 2;
+    // cout<<student;
+
+    // student = student%2;
+    student %= 2;
+    cout<<student;
+}
