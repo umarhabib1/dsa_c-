d95c7e550e7d5308081dede13692fd249e93c012
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-02-06 22:32:41 +0500

    "math_related_functions_in C++"

---
+++ C++/math_related_functions_in C++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x = 3.9;
+    double y = 4;
+    double z;
+
+    // z = std::max(x,y);
+    // z = std::min(x,y);
+    // z = pow(x,2);
+    // z = pow(5,2);
+    // z = sqrt(9);
+    // z = sqrt(8);
+    // z =abs(-99);
+    // z = round(x);
+    // z = ceil(x);
+    // z = floor(x);
+    cout << z;
+}
