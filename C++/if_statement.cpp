c7b45d16cb23358e36107a2a09f0f55b63912807
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-02-08 21:32:43 +0500

    "if_statement"

---
+++ C++/if_statement.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+// If statements
+
+
+// if (condition)
+// {
+//     /* code */
+// }
+
+
+// problem 1
+
+// int number;
+// cout<<"Enter a number:  ";
+// cin>>number;
+
+// if (number == 0)
+// {
+//     cout<<"It is a zero";
+// }else if(number > 0){
+//     cout<<"it is a positive number";
+// }else if(number < 0){
+//     cout<<"it is a negative number";
+// }
+
+// }
+
+
+
+// problem 2
+
+// int number;
+// cout<<"Enter a number:  ";
+// cin>>number;
+
+// if (number%2 == 0)
+// {
+//     cout<<"It is a even number";
+// }else{
+//        cout<<"It is a odd number";
+// }
+
+
+// problem 3
+
+// int Marks;
+// cout<<"Enter your Marks:   " ;
+// cin>>Marks;
+
+// if (Marks >= 90 && Marks <= 100 )
+// {
+//     cout<<"your Grade is  A";
+// }
+// else if (Marks <= 89 && Marks >= 80)
+// {
+//     cout<<"your Grade is  B";
+// }else if (Marks <= 79 && Marks >=70)
+// {
+//     cout<<"your Grade is  C";
+// }
+// else if (Marks <= 69 && Marks >=60)
+// {
+//     cout<<"your Grade is  D";
+// }else if (Marks > 100)
+// {
+//  cout<<"out of range";
+// }else if (Marks < 0)
+// {
+//     cout<<"erro Negative numbers detected";
+// }else{
+//     cout<<"you are Fail try again";
+// }
+
+
+// Problem 4
+
+// int age;
+// cout<<"Enter your age:   " ;
+// cin>>age;
+
+// if (age < 18)
+// {
+//     cout<<"Not eligible to vote";
+// }else if (age >=18)
+// {
+//     cout<<"Eligible to vote";
+// }
+
+
+// Problem 5
+
+// int number1;
+// int number2;
+// int number3;
+// cout<<"enter first number: ";
+// cin>>number1;
+// cout<<"enter second number: ";
+// cin>>number2;
+// cout<<"enter third number: ";
+// cin>>number3;
+
+// if (number1 > number2 && number1 > number3)
+// {
+//     cout<<"first number is the largest number";
+// }else if (number2 > number1 && number2 > number3)
+// {
+//     cout<<"second  number is the largest number";
+// }else if (number3 > number1 && number3 > number2)
+// {
+//     cout<<"third number is the largest number";
+// } else if (number1 == number2 && number2 == number3)
+// {
+//     cout<<"All numbers are equal";
+// }
+
+
+
+
+// Problem 6
+
+// int year;
+// cout<<"Enter a year:   " ;
+// cin>>year;
+
+// if (year%400 == 0)
+// {
+//     cout<<"Leap year";
+// }else if (year%4 == 0 && year%100 != 0)
+// {
+//     cout<<"Leap year"; 
+// }else{
+//     cout<<"Not a Leap year";
+// }
+
+
+// Problem 7
+
+// int number;
+// cout<<"Enter a number:   " ;
+// cin>>number;
+
+
+// if (number % 3 == 0 && number % 5 == 0) {
+//     cout << "FizzBuzz";
+// }
+// else if (number % 3 == 0) {
+//     cout << "Fizz";
+// }
+// else if (number % 5 == 0) {
+//     cout << "Buzz";
+// }
+// else {
+//     cout << number;
+// }
+
+
+
+}
