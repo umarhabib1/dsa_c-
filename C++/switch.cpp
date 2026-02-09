853f76dc81feab770a5f6af89f80cfeb1275d089
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-02-09 22:36:36 +0500

    "switch_in_C++"

---
+++ C++/switch.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //Problem 1
+// Input a number (1–7).
+// Print the day of the week.
+
+// int day;
+//   cout<<"Enter number from 1 to 7: ";
+//   cin>>day;
+
+//   switch (day)
+//   {
+//   case  1:
+//     cout<<"Monday";
+//     break;
+//   case  2:
+//     cout<<"Tuesday";
+//     break;
+//   case  3:
+//     cout<<"wednesday";
+//     break;
+//   case  4:
+//     cout<<"Thurstday";
+//     break;
+//   case  5:
+//     cout<<"Friday";
+//     break;
+//   case  6:
+//     cout<<"Saturday";
+//     break;
+//   case  7:
+//     cout<<"sunday";
+//     break;
+  
+//   default:
+//   cout<<"plz enter a number from 1 to 12";
+//     break;
+//   }
+
+ // Problem 2
+
+// char Alphabet;
+//   cout<<"Enter a Alphabet : ";
+//   cin>>Alphabet;
+
+//   switch (Alphabet)
+//   {
+//   case 'A':
+//     cout<< Alphabet<<" is a vowel";
+//     break;
+//   case 'E':
+//     cout<< Alphabet<<" is a vowel";
+//     break;
+//   case 'I':
+//     cout<< Alphabet<<" is a vowel";
+//     break;
+//   case 'O':
+//     cout<< Alphabet<<" is a vowel";
+//     break;
+//   case 'U' :
+//     cout<< Alphabet<<" is a vowel";
+//     break;
+//   case 'a':
+//     cout<< Alphabet<<" is a vowel";
+//     break;
+//   case 'e':
+//     cout<< Alphabet<<" is a vowel";
+//     break;
+//   case 'i':
+//     cout<< Alphabet<<" is a vowel";
+//     break;
+//   case 'o':
+//     cout<< Alphabet<<" is a vowel";
+//     break;
+//   case 'u' :
+//     cout<< Alphabet<<" is a vowel";
+//     break;
+  
+//   default:
+//   cout<<Alphabet<<" is a consonant  ";
+//     break;
+//   }
+
+
+ // Problem 3
+// int number1;
+// int number2;
+// int number3;
+//  cout<< "Enter number 1: ";
+//  cin>>number1;
+//  cout<< "Enter number 2: ";
+//  cin>>number2;
+ 
+//  cout<< "Enter 1 for addition ,  2 for subtraction , 3 for multipication , 4 for divition  ";
+//  cin>>number3;
+ 
+//  switch (number3)
+//  {
+//  case 1:
+//     cout<<number1<< " + " << number2 <<" = " << number1 +number2;
+//     break;
+//  case 2:
+//     cout<<number1<< " - " << number2 <<" = " << number1 - number2;
+//     break;
+//  case 3:
+//     cout<<number1<< " * " << number2 <<" = " << number1 * number2;
+//     break;
+//  case 4:
+//     cout<<number1<< " / " << number2 <<" = " << number1 / number2;
+//     break;
+ 
+//  default:
+//  cout<<"plz enter from 1 to 4 to choose the operation you want";
+//     break;
+//  }
+
+
+// problem 4 
+
+// problem four is just like Problem 1 so i do not want to write it again
+
+
+
+// problem 5
+
+// char Alphabet;
+// cout<<"Enter a Alphabet; R , G or B ; only in upper case : ";
+// cin>>Alphabet;
+
+// switch (Alphabet)
+// {
+// case 'R':
+//     cout<<"Red";
+//     break;
+// case 'G':
+//     cout<<"Green";
+//     break;
+// case 'B':
+//     cout<<"Blue";
+//     break;
+
+// default:
+// cout<<"plz enter only  R , G or B;  and in uppercase";
+//     break;
+// }
+
+
+}
