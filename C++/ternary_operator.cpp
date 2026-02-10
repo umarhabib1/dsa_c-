1931d33ab42e02638c92341b04f74c89cf6be23b
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-02-10 22:51:11 +0500

    "ternary_operator_in_C++"

---
+++ C++/ternary_operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // ternary operator  ?   :
+
+    // int age =12;
+    // age >= 18 ? cout<<"you can drive" : cout<< "you can not drive";
+
+    // int num = 10;
+    // num%2 == 0 ? cout<<"even number" : cout<<"odd number";
+    
+    int num = 10;
+   cout<<(num%2 == 0 ? "even number" : "odd number");
+   
+   return 0;
+    
+}
