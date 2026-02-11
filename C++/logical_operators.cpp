ec1c6eca873a7876536b16470e18b6c9bcb41b41
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-02-11 22:21:28 +0500

    "logical_operators"

---
+++ C++/logical_operators.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //  &&  = check if two condition are true
+    // ||   = check if at least one of two conditions is true
+    //  !   = reverses  the logical state of its operand
+   
+    // int temp;
+    // cin>>temp;
+
+    // if (temp >= 0 && temp <= 30)
+    // {
+    //     cout<<"temprature is good";       
+    // }else{
+    //     cout<<"temprature is not good";
+    // }
+    
+    //  int temp;
+    // cin>>temp;
+    // if (temp <= 0 || temp >= 30)
+    // {
+    //  cout<<"temprature is  not good";       
+
+    // }else{
+    //     cout<<"temprature is good";
+    // }
+
+    
+    bool day = true;
+
+    if (day != false)
+    {
+        cout<< "its day time";
+    }else{
+        cout<< "its night time";
+        
+    }
+    
+        
+}    
