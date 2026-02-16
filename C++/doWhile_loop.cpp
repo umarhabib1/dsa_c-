4aba7da69a279e964d66d2ee94873e172ab76ed5
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-02-16 21:33:15 +0500

    "doWhile loop"

---
+++ C++/doWhile_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //  do while loop = do some block of code first,
+    //                  then repeat again if condition is true  
+
+   int number;
+
+   do
+   {
+    cout<<"Ener a positive number";
+    cin>>number;
+   } while (number < 0);
+   
+}
