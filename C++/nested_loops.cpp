d9d4388734838f9b5a7ffa1d46c448eca6db1a5f
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-02-18 19:09:31 +0500

    "nested loops"

---
+++ C++/nested_loops.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+// for (int i = 0; i < 10; i++)
+// {
+//     for (int j = 0; j < 10; j++)
+//     {
+//         cout<<i;
+//     }   
+// }
+
+
+
+
+    // int rows,col;
+    // char symbol;
+    // cout<<"How many rows";
+    // cin>>rows;
+    // cout<<"How many coloumn";
+    // cin>>col;
+    // cout<<"Enter a smbol to use";
+    // cin>>symbol;
+
+    // for (int i = 0; i < rows; i++)
+    // {
+    //         for (int j = 0; j < col; j++)
+    //         {
+    //             cout<<symbol;
+    //         }
+    //         cout<<endl;
+    // }
+    
+}
