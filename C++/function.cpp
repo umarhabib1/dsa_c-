2d329a3e21c70db9d5f6a37d069c89476f20e993
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-02-22 22:44:26 +0500

    "Functions in c++"

---
+++ C++/function.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+using namespace std;
+
+// void birthday(string name, int num){
+//     cout<<"Happy birthy day "<<name<<endl;
+//     cout<<"Happy birthy day "<<name<<endl;
+//     cout<<"Happy birthy day "<<name<<endl;
+//     cout<<"my age is "<<num;
+// }
+
+// int addnum(int a, int b)
+// {
+//     return a + b;
+// }
+
+
+// int Maximum_num(int a,int b){
+// if (a>b)
+// {
+//     return a;
+// }else if (a == b)
+// {
+//     return a, b;
+// }
+// {
+//     return b;
+// }
+
+// }
+
+
+
+// int factorial_num(int a){
+//     int result= 1;
+//   for (int i = 1; i <= a; i++)
+//   {
+//        result *=i;
+        
+//   }
+  
+//   return result;
+// }
+    
+
+// int  prime_num(int a){
+    
+// if (a%a==0)
+// {
+//     cout<<a<<" is a  prime number";
+// }else if (a ==0)
+// {
+//     cout<<a<<" is not a prime number";
+// }else{
+//     cout<<a<<" is not a prime number";
+// }
+ 
+// }
+
+
+int reversed_num(){
+    
+}
+
+
+
+int main()
+{
+    // function = a block of reusable code
+    // string name = "bro";
+    // int age = 90;
+    // birthday(name,age);
+
+    //   problem 1  Create a function that takes two integers and returns their sum.
+    //     int x = 4;
+    //     int z =13;
+    //     int sum =addnum(x,z);
+    //    cout<<sum;
+
+
+
+
+    //problem 2  Create a function that takes two numbers and returns the bigger one.
+
+    // int x = 14;
+    //     int z =13;
+    //     int max =Maximum_num(x,z);
+    //     cout<<max;
+
+
+
+
+    // problem 3: Write a function that takes a number and returns its factorial.
+
+        // int x = 5;
+        // int factorial = factorial_num(x);
+        // cout<<factorial;
+
+
+
+
+
+        //  problem 4: Create a function that takes a number and returns true if it is prime, otherwise false
+        
+        // int x = 7;
+        // int prime = prime_num(x);
+        // cout<<prime;
+        
+
+
+
+
+        // problem 5: Create a function that takes a number and returns the reversed number.
+
+        int x = 7;
+
+        
+
+
+
+    return 0;
+}
