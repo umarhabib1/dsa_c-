3978b6ad1e8b3df58f9e12567523767d483f723f
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-02-28 18:04:57 +0500

    "arrays in c++"

---
+++ C++/arrays.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // arrays = a data structure that can hold multiple values
+    // vlaues are accessed by index number
+    // indx starts from 0 to 1,2,3......n
+
+    // string cars[] ={"bugaty","mustang"};
+    // cars[1] ="camry";
+    // cout<< cars[0] <<endl;
+    // cout<< cars[1]<<endl;
+
+    string furit[2];
+    furit[0]= "mango" ;
+    furit[1]="apple" ;
+     cout<< furit[0] <<endl;
+    cout<< furit[1]<<endl;
+  
+    return 0;
+}
