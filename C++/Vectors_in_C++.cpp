c7c24c6326220782faf9a2ad8269e2a8229cfcda
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-03-12 23:02:30 +0500

    "Vectors in C++"

---
+++ C++/Vectors_in_C++.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    /*
+    A vector is a dynamic array fsrom the <vector> library.
+
+👉 Unlike normal arrays:
+
+Size can grow or shrink
+
+Safer and easier to use
+
+Has many built-in functions
+    */
+
+    /*
+    push_back()	Add element
+
+    pop_back()	Remove last element
+
+    size()	Number of elements
+
+    empty()	Check if empty
+
+    clear()	Remove all elements
+
+    front()	First element
+
+    back()	Last element
+    */
+
+
+    vector <int> vec = {10,20,30};
+
+
+    // Methods for vectors
+
+    // vec.push_back(40);
+    // vec.pop_back();
+    // cout<< vec.size();
+    // vec.empty();
+    // vec.clear();
+    // cout<< vec.front();
+    // cout<< vec.back();
+
+
+    
+    // for (int i = 0; i < vec.size(); i++)
+    // {
+    //     cout<< vec[i]<<" ";
+    // }
+    
+
+
+}
