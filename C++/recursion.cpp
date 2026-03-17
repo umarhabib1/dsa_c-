b43005f745e58a2ae5c2d0bd6093dbf20a2b219f
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-03-17 18:12:51 +0500

    "recursion in C++"

---
+++ C++/recursion.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+// int factorial(int num){
+//      // Base case
+// if (num == 0 || num == 1)
+// {
+//     return 1;
+// }
+// // Recursive call
+//   return num *(factorial(num-1));
+
+// }
+
+// void print(int n){
+
+//     if(n == 0)
+//         return;
+
+//     cout << n << endl;
+
+//     print(n - 1);
+// }
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+// int sumNum(int num){
+//     int sum = 0;
+//     if (num == 0)
+//     {
+//      return 0;
+//     }
+//       return num + sumNum(num-1);
+
+// }
+
+
+int reverseNum(int num){
+   int last,first;
+    last =  num%10;
+
+
+}
+
+
+int main(){
+    /*
+     Recursion is when a function calls itself to solve a smaller version of the same problem.
+
+Simple words:
+A function that repeats itself until a stopping condition is met.
+*/
+
+// cout<<factorial(5)<<endl;
+// print(5);
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+//  problem 1: Sum of First n Numbers
+// int n = 3;
+//  cout<< sumNum(n);
+
+
+//  problem 2: Reverse a Number
+int n = 1234;
+   reverseNum(n);
+
+}
