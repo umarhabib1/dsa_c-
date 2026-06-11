bdb9248f2694a14ab3ff9e24c9d9907ba99ce8e9
umarhabib1 <mumarhabib1@gmail.com>
2026-06-11 11:44:43 -0700

    "Pow X_N_Power exponential Problem"

---
+++ DSA/Pow_X_N_Power_exponential_Problem.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+//      double x = 3;
+//     int n = -1;
+//     double ans = 1;
+//     long long binaryForm = n;
+
+//     if (n < 0)
+//     {
+//         x = 1/x;
+//         binaryForm = - binaryForm;
+//     }
+    
+    
+    
+//     while (binaryForm > 0)
+//     {
+//         if (binaryForm % 2 == 1)
+//         {
+//             ans *= x;
+//         }
+//          x *=x;
+//         binaryForm /= 2;
+//     }
+//   cout<<ans;   
+ 
+
+
+
+    /////////////////////// Buy and Sell Stock Problem  ///////////////////////////////
+
+    // vector<int> prices ={7,1,5,3,6,4};
+    // int maxPrice = 0;
+    // int bestBuy = prices[0];
+
+    // for (int i = 1; i < prices.size(); i++)
+    // {
+    //     if (prices [i] > bestBuy)
+    //     {
+    //         maxPrice = max(maxPrice, prices[i] - bestBuy);
+    //     }
+        
+    //     if (prices[i] < bestBuy)
+    //     {
+    //         bestBuy = prices[i];
+    //     }
+        
+    // }   
+    // cout<<maxPrice;
+
+    
+    //  vector<int> prices = {7,1,5,3,6,4};
+
+    // int bestBuy = prices[0];
+    // int maxProfit = 0;
+
+    // for (int i = 1; i < prices.size(); i++) {
+
+    //     bestBuy = min(bestBuy, prices[i]);
+
+    //     maxProfit = max(maxProfit, prices[i] - bestBuy);
+    // }
+    // cout << maxProfit;
+
+
+}
