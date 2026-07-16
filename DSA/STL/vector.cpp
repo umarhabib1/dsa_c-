a2edd275e8ddc919a25e50a9d65abcf69a9ef3b8
umarhabib1 <mumarhabib1@gmail.com>
2026-07-16 11:47:57 -0700

    "vector"

--- DSA/STL/vector.cpp
+++ DSA/STL/vector.cpp
@@ -3,17 +3,48 @@
 using namespace std;
 
 int main(){
-    vector<int> vec = {1,2,3};
-    cout<<"size = "<<vec.size()<<endl;
-    cout<<"capacity = "<<vec.capacity()<<endl;
-    vec.push_back(4);
-    vec.push_back(5);
-    vec.emplace_back(6);
-    vec.pop_back();
-    cout<<"size = "<<vec.size()<<endl; 
-    cout<<"capacity = "<<vec.capacity()<<endl;
+    // vector<int> vec = {1,2,3};
+    // cout<<"size = "<<vec.size()<<endl;
+    // cout<<"capacity = "<<vec.capacity()<<endl;
+    // vec.push_back(4);
+    // vec.push_back(5);
+    // vec.emplace_back(6);
+    // vec.pop_back();
+    // cout<<"size = "<<vec.size()<<endl; 
+    // cout<<"capacity = "<<vec.capacity()<<endl;
+    
+    // cout<<"vec at index of 2 = "<<vec.at(2)<<endl;
+    // cout<<"vec at index of 3  = "<<vec[3]<<endl;
+    
 
-    cout<<"vec at index of 2 = "<<vec.at(2)<<endl;
-    cout<<"vec at index of 3  = "<<vec[3]<<endl;
+    // vector<int> v = {1,2,3,4,5,6,7};
+    
+    // v.erase(v.begin() ); // removes the first element
+    // v.erase(v.begin() + 2); // removes the third element
+    // v.erase(v.begin() + 2, v.end() - 1); // removes the third element
+    
+    // v.insert(v.begin()+2,3,9);
+    
+    // for (int val : v)
+    // {
+        //     cout<<val;
+        // }
+        
+
+        vector<int> v = {1,2,3,4,5,6,7};
+        // forward
+        // for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        // {
+        //     cout<<*(it);
+        // }
+
+
+        // backwards
+        // for (auto it = v.rbegin(); it != v.rend() ; it++)    
+        // {
+        //     cout<<*(it);
+        // }
+        
+        
 
 }
