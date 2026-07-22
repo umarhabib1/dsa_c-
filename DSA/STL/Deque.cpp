8bc4ec1693d736f4ecf59e61ed1c49e7ebd23c44
umarhabib1 <mumarhabib1@gmail.com>
2026-07-22 11:40:04 -0700

    "Deque in C++ STL"

--- DSA/STL/Deque.cpp
+++ DSA/STL/Deque.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <deque>
 using namespace std;
- 
-int main(){
-     
-    
+
+int main()
+{
+    deque<int> d = {10, 20, 30};
+
+    d.push_back(10);
+    d.push_back(20);
+    d.push_front(5);
+
+    cout<<d[2]<<endl;
+
+    for (auto it = d.begin(); it != d.end(); it++)
+    {
+        cout << *it << " ";
+    }
 }
