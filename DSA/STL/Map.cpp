d211cf95699e253886e1ce29023894989de5a707
umarhabib1 <mumarhabib1@gmail.com>
2026-07-29 11:42:57 -0700

    "Map"

--- DSA/STL/Map.cpp
+++ DSA/STL/Map.cpp
@@ -3,5 +3,11 @@
 using namespace std;
 
 int main(){
+    map<string,int> students;
+    students.insert({"umar",1});
+    students.insert({"hassan",2});
+    students.insert({"haider",3});
+    
+    cout << students["umar"];
 
 }
--- DSA/STL/Priority_Queue.cpp
+++ DSA/STL/Priority_Queue.cpp
@@ -2,16 +2,28 @@
 #include <queue>
 using namespace std;
 
-int main(){
-    priority_queue<int> q1;
+int main()
+{
+    priority_queue<int> q1;  // max queue
     q1.push(1);
     q1.push(2);
     q1.push(3);
     q1.push(4);
-    
+
     q1.pop();
 
-    cout<<q1.size()<<endl;
+    cout << q1.size() << endl;
 
- 
+    priority_queue<int, vector<int>, greater<int>> pq;  // min queue
+
+    pq.push(30);
+    pq.push(10);
+    pq.push(50);
+    pq.push(20);
+
+    while (!pq.empty())
+    {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
 }
