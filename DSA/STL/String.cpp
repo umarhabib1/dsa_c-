143f1635b560887edfdbe4bfdfc14b2eec2b99ed
umarhabib1 <mumarhabib1@gmail.com>
2026-08-03 10:48:01 -0700

    "Strings in C++"

--- DSA/STL/String.cpp
+++ DSA/STL/String.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() { 
-    
- 
+    string s1 = "hello";
+    cout<<"s1 size = "<<s1.size()<<endl;
+    cout<<"s1 length = "<<s1.length()<<endl;
+    cout<<"s1[1] = "<<s1[1]<<endl;
+
+    string s2 = "My";
+    string s3 = " name";
+    cout<<s2 + s3<<endl;
+
 }
