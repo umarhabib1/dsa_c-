72d81dcf65586ca8e0581ed4aebcbfbd1c6aba67
umarhabib1 <mumarhabib1@gmail.com>
2026-08-04 11:56:20 -0700

    "Valid Palindrome and Remove all Occurrences"

---
+++ DSA/STL/Valid_Palindrome_and_Remove_all_Occurrences.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// void Reverse(string str,string &strReverse){
+//     int st =0, end = str.size()-1;
+//     while (st < end)
+//     {
+//         swap(str[st], str[end]);
+//         st++ , end--;
+//     }
+//     strReverse = str;
+// }
+
+// void Check(string str,string strReverse){
+//     if (str == strReverse)
+//     {
+//         cout<<"is Palindrome"<<endl; 
+//     }else{
+//         cout<<"Not a Palindrome"<<endl; 
+//     }
+    
+// }
+
+int main() { 
+    // string str = "madam";
+    // string strReverse; 
+    
+
+    // Reverse(str, strReverse);
+    // Check(str, strReverse);
+
+    // cout<<strReverse<<endl;
+
+}
