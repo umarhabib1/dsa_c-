8cd1e5479537154058aeafb4713aa84648031954
umarhabib1 <mumarhabib1@gmail.com>
2026-08-07 11:53:55 -0700

    "Maths for DSA"

---
+++ DSA/Maths_for_DSA.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+// bool isPrime(int n)
+// {
+//     if (n < 1)
+//     {
+//         return false;
+//     }
+
+//     for (int i = 2; i * i < n; i++)
+//     {
+//         if (n % 2 == 0)
+//             return false;
+//     }
+//         return true;
+// }
+
+void printDight(int n){
+    while (n != 0)
+    {
+        int digit = n % 10;
+        cout<<"num = "<<digit<<endl;
+
+        n = n/10;
+    }
+    
+}
+
+int main()
+{
+    //  int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // if (isPrime(n))
+    //     cout << n << " is a Prime Number";
+    // else
+    //     cout << n << " is Not a Prime Number";
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    printDight(n);
+
+}
