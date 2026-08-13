9c10030ff266b4e63387326f7f5227cabf78cafd
umarhabib1 <mumarhabib1@gmail.com>
2026-08-13 11:35:12 -0700

    "2D Vectors in C++"

--- DSA/2D_Arrays_in_C++.cpp
+++ DSA/2D_Arrays_in_C++.cpp
@@ -1,21 +1,86 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int matrix[3][3] = {{1,2,3},
-                        {4,5,6},
-                        {7,8,9}};
+int main()
+{
+    // int matrix[3][3] = {{1, 2, 3},
+    //                     {4, 5, 6},
+    //                     {7, 8, 9}};
 
-    int rows = 3, col = 3;
+    // int rows = 3, col = 3;
 
-    for (int i = 0; i < rows; i++)
+    // for (int i = 0; i < rows; i++)
+    // {
+    //     for (int j = 0; j < col; j++)
+    //     {
+    //         cout<<matrix[i][j];
+    //     }
+    //      cout<<endl;
+    // }
+
+    // linear search
+    // int key = 4;
+    // bool found = false;
+
+    // for (int i = 0; i < rows; i++)
+    // {
+    //     for (int j = 0; j < col; j++)
+    //     {
+    //         if (matrix[i][j] == key)
+    //         {
+    //             cout << "Found at: " << i << "," << j << endl;
+    //             found = true;
+    //         }
+    //     }
+    // }
+
+    // if (!found)
+    // {
+    //     cout << "Not found";
+    // }
+
+    //
+    // int matrix[3][3] = {{1, 2, 3},
+    //                     {4, 5, 6},
+    //                     {7, 8, 9}};
+
+    // int rows = 3, col = 3;
+
+    // int Maxsum = INT_MIN;
+    // for (int i = 0; i < rows; i++)
+    // {
+    //     int sum = 0;
+    //     for (int j = 0; j < col; j++)
+    //     {
+    //         sum += matrix[i][j];
+    //     }
+    //     if (Maxsum < sum)
+    //     {
+    //         Maxsum = sum;
+    //     }
+    // }
+
+    // cout << "Max sum = " << Maxsum;
+
+    int matrix[4][4] = {{1, 2, 3, 4},
+                        {5, 6, 7, 8,},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}};
+    int n = 4;
+    int diagonalSum = 0;
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < col; j++)
+        for (int j = 0; j < n; j++)
         {
-            cout<<"matrix at row = "<<i<<" matrix at colum = "<<j<<" " <<matrix[i][j]<<endl;
+            if (j == i)
+            {
+                diagonalSum += matrix[i][j];
+            }
+            else if (j == n - 1 - i)
+            {
+                diagonalSum += matrix[i][j];
+            }
         }
-        
-        
     }
-    
+    cout<<diagonalSum<<endl;
 }
---
+++ DSA/2D_Vectors_in_C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<vector<int>> matrix = {{1,2,3},{4,5,6}};
+    // rows = matrix.size();
+    // col = matrix[i].size();
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix[i].size(); j++)
+        {
+            cout<<matrix[i][j];
+        }
+        
+    }
+    
+}
