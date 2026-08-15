97b4ac001ecece04252a900554e3df5efe885641
umarhabib1 <mumarhabib1@gmail.com>
2026-08-15 11:54:41 -0700

    "Recursion "

--- DSA/Recursion.cpp
+++ DSA/Recursion.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){}
+void printNumber(int n){
+if (n == 1)    // base case
+{
+    cout<<1<<" ";      
+    return;
+}
+
+printNumber(n-1); // recursive case
+cout<<n<<" ";
+
+}
+int main(){
+    printNumber(5);
+}
