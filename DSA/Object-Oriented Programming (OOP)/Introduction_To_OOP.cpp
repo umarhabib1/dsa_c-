a2d97e7a5e1bc05252842bb33778b526abca4c6a
umarhabib1 <mumarhabib1@gmail.com>
2026-08-25 11:57:28 -0700

    "Introduction To OOP"

---
+++ DSA/Object-Oriented Programming (OOP)/Introduction_To_OOP.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    OOP is a programming paradigm that organizes programs using classes and objects,
+     combining data and functions together.
+    */
+}
