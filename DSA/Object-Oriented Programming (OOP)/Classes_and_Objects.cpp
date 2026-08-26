dc7824d58925eeabb50787b984a722cb775e9633
umarhabib1 <mumarhabib1@gmail.com>
2026-08-26 11:49:47 -0700

    "Classes and Objects"

---
+++ DSA/Object-Oriented Programming (OOP)/Classes_and_Objects.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    // properties
+    string name;
+    int age;
+
+    //  methods  / Member functions
+    void print()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main()
+{
+    Student Student1 = {"Umar", 999};
+    Student1.print();
+
+    Student Student2;
+    Student2.age = 41;
+    Student2.name = "Nangyal";
+    Student2.print();
+}
