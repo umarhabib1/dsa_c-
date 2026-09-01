c6db1491768757ce46b82020793507d80e3791d9
umarhabib1 <mumarhabib1@gmail.com>
2026-09-01 11:55:46 -0700

    "Constructors in C++"

---
+++ DSA/Object-Oriented Programming (OOP)/Constructors_in_C++.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Teacher
+{
+private:
+    int salary;
+
+public:
+    void setSalary(int s)
+    {
+        salary = s;
+    }
+
+    int getSalary()
+    {
+        return salary;
+    }
+};
+int main()
+{
+    Teacher t;
+
+    t.setSalary(50000);    // ✅
+    cout << t.getSalary(); // ✅
+
+    // t.salary = 50000; // ❌ wil give error
+    
+}
