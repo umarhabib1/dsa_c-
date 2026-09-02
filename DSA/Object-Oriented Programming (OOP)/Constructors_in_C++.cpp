92afae1aed88543ffc414e79403f6fab6e16bdac
umarhabib1 <mumarhabib1@gmail.com>
2026-09-02 11:51:03 -0700

    "Constructors"

--- DSA/Object-Oriented Programming (OOP)/Constructors_in_C++.cpp
+++ DSA/Object-Oriented Programming (OOP)/Constructors_in_C++.cpp
@@ -1,28 +1,7 @@
 #include <iostream>
 using namespace std;
-class Teacher
-{
-private:
-    int salary;
 
-public:
-    void setSalary(int s)
-    {
-        salary = s;
-    }
-
-    int getSalary()
-    {
-        return salary;
-    }
-};
 int main()
 {
-    Teacher t;
-
-    t.setSalary(50000);    // ✅
-    cout << t.getSalary(); // ✅
-
-    // t.salary = 50000; // ❌ wil give error
-    
+ cout<<"hi";   
 }
