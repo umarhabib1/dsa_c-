74c16f66032b06f1de55a025c4b35d8a87cf728c
umarhabib1 <mumarhabib1@gmail.com>
2026-09-07 11:49:43 -0700

    "Constructors C++"

--- DSA/Object-Oriented Programming (OOP)/Constructors_in_C++.cpp
+++ DSA/Object-Oriented Programming (OOP)/Constructors_in_C++.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
     int a = 5;
     int b = 4;
     int c = a+b;
+    string s =  "hello";
     
-    cout<<c;  
+    cout<<c<<endl;  
+    cout<<s<<endl;  
 }
